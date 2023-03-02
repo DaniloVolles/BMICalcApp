9bc66e010d1d6f510dd56f755106581eaa7a96bf
Danilo Volles <danilo.volles@outlook.com>
2023-03-02 18:27:02 -0300

    InsertHeight and insertWeight functions implementation.

--- app/src/main/java/com/example/bmicalcapp/MainActivity.kt
+++ app/src/main/java/com/example/bmicalcapp/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.bmicalcapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.bmicalcapp.ui.theme.BMICalcAppTheme
 
@@ -22,11 +25,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column (modifier = Modifier.padding(all = 10.dp)) {
+
+                        val height = insertHeight()
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        val weight = insertWeight()
+                    }
                 }
             }
         }
     }
+}
+
+@Composable
+fun insertHeight (): TextFieldValue {
+    var text by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = {
+                Text (text = "Height")
+            },
+            placeholder = {
+                Text (text = "Insert your height in Meters")
+            }
+        )
+    return text
+}
+
+@Composable
+fun insertWeight (): TextFieldValue {
+    var text by remember {
+        mutableStateOf(TextFieldValue(""))
+    }
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = {
+                Text (text = "Weight")
+            },
+            placeholder = {
+                Text (text = "Insert your weight in Kilograms")
+            }
+        )
+    return text
 }
 
 @Composable
